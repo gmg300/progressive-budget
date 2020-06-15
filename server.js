4a754acdfef613cfee7f2e2e2d17ed45c7687b68
Garrett <garrettgassensmith@gmail.com>
2020-06-15 18:40:16 -0500

    changed PORT const to process.env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,8 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGO_URI, {
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
